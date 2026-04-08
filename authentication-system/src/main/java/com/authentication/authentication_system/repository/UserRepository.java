a517c669e163f3c85a0a924e74805c552ab7b95a
Harshith-Raj-Bangera <harshith.raj@mphasis.com>
2026-04-08 22:27:58 +0530

    Delete authentication-system directory

--- authentication-system/src/main/java/com/authentication/authentication_system/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.authentication.authentication_system.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.authentication.authentication_system.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
